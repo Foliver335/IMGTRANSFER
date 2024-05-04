5e26c703ede1cea68b047186a8f15e58075ddd82
Felipe Fause <79032229+Foliver335@users.noreply.github.com>
2024-05-04 15:34:15 -0300

    Create image_view.py

---
+++ src/views/image_view.py
@@ -0,0 +1,8 @@
+class ImageView:
+    @staticmethod
+    def display_success(message):
+        print(f'Sucesso: {message}')
+
+    @staticmethod
+    def display_error(message):
+        print(f'Erro: {message}')
